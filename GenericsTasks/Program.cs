26d1a66fe4c0f6d46f338ec5eccd8f5068abdd0a
Daniel Min Tibbotts <dtib201@mywhitecliffe.com>
2021-02-26 13:36:25 +1300

    Create Visual Studio Files

---
+++ GenericsTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
